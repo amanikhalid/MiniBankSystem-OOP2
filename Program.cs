bfca72cd973f4dbe18aa5b3e62696cc29b5b26c9
Amani Khalid <amanikhalidd@gmail.com>
2025-07-16 08:55:17 +0400

    Inserted Task Requirements

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace MiniBankSystem_OOP2
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MiniBankSystem_OOP2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //  Solve the minibank system with oop part1
+
+            //step 1: determine the main class ex : user or patient or visitor
+            //step 2: decide which data will be primitive and which user defined and which is single / list
+            // step 3: implement all the user defined classes needed
+            // step 4: keep repeating 2,3 till all classes have only primitive
+            // step 5: complete all other seperate classes needed
+            // step 6: we are now defined all data needed and the next step is to determine the access modifiers / and static
+            //step 7: for private data fields implement public methods to set and get them
+            //step 8: implemet methods which have logic
+            //step 9: implement Main program method with it's logic (instantiate system objects )
+
+
+
+
+
         }
     }
 }
+
+
+
+
+
